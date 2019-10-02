05aba709999e71e897c846faa93da41d594be0df
James Grantham <james.r.grantham@gmail.com>
2019-10-02 13:33:30 +0100

    Build setValue for local storage

--- src/index.js
+++ src/index.js
@@ -12,8 +12,18 @@ const useLocalStorage = (key, initialValue) => {
     const item = window.localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
   });
-  return [storedValue];
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  return [storedValue, setValue];
 };
+
+const useDarkMode = () => {
+  
+}
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
